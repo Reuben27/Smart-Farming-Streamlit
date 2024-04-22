4f155b4c5b204671d9990537c95677af5256b0f4
Reuben27 <reuben.sd@iitgn.ac.in>
2024-04-22 16:18:25 +0530

    Upload Codes

--- pages/1_Multispectral_Imaging.py
+++ pages/1_Multispectral_Imaging.py
@@ -8,6 +8,7 @@ import rasterio
 import numpy as np
 # import matplotlib.pyplot as plt
 
+path = "https://github.com/Reuben27/Smart-Farming-Streamlit/raw/main/images/"
 try:
   # Load a model
   model = YOLO(".\\pages\\best.pt") 
@@ -15,9 +16,9 @@ try:
 except:
   model = None
 
-red_band_path = ".\\images\\red_band.TIF"
-green_band_path = ".\\images\\green_band.TIF"
-nir_band_path = ".\\images\\nir_band.TIF"
+red_band_path = path + "red_band.TIF"
+green_band_path = path + "green_band.TIF"
+nir_band_path = path + "nir_band.TIF"
 
 def object_detection(image):
   results = model([image])  # return a list of Results objects
@@ -68,17 +69,21 @@ def reconstruction():
 
 
 st.title("Multispectral Imagery")
+st.write("This project delves into the transformative potential of smart farming in organic agriculture, utilizing drone-based aerial imagery. The study aims to construct a detailed representation of the farm terrain and its vegetation cover. By generating a 3D model of the farm terrain and calculating key vegetation indices such as NDVI, NDWI, LAI, Vegetation Cover Fraction, and Soil Moisture Index (SMI), our study aims to provide detailed insights into crop health and vitality. Emphasizing techniques such as index calculation, channel integration, and image super-resolution, we aim to offer a comprehensive toolset for optimizing agricultural management practices. Additionally, the exploration of SAHI for small object detection aids in accurately identifying and monitoring minute features within the agricultural landscape.")
 uploaded_files = st.file_uploader("Upload an Image", type=["png", "jpg", "jpeg", "JPG"], accept_multiple_files=True)
-print("Hey")
 
-Object_Detection = st.button("Do Object Detection")
+col_1, col_2, col_3 = st.columns(3)
+with col_1:
+  agree = st.checkbox("Use Default Images")
+with col_2:
+  Object_Detection = st.button("Do Object Detection")
+with col_3:
+  Reconstruction = st.button("Do reconstruction")
 
-Reconstruction = st.button("Do reconstruction")
-
-if Reconstruction:
+if agree and Reconstruction:
   reconstruction()
 
-if Object_Detection and model is not None:
+if Object_Detection and model is not None and not agree:
   if uploaded_files is not None:
     for uploaded_file in uploaded_files:
       print(uploaded_file)
@@ -93,5 +98,5 @@ if Object_Detection and model is not None:
 
       st.subheader("Output Image")
       # st.image(modified_img_array, clamp=True, channels='BGR')
-
-  
+elif Object_Detection:
+  st.image(path + "test1.jpg")
